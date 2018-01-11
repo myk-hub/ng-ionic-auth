6cea93ae25bbd59e184a63c29f390bc1ab64493d
usernamen7 <n7username7@gmail.com>
2018-01-11 14:30:47 +0200

    generete Login, Register pages

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBJdXhGQzO-sG7bkqETY9BE2MuteAuUhXE",
+    authDomain: "ng-auth-46449.firebaseapp.com",
+    databaseURL: "https://ng-auth-46449.firebaseio.com",
+    projectId: "ng-auth-46449",
+    storageBucket: "ng-auth-46449.appspot.com",
+    messagingSenderId: "722616566641"
+};
